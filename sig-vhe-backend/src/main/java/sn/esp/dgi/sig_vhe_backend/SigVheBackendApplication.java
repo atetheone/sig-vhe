484d3959b34bcff47bc53313bf618255da481ed3
atetheone <atevirran@gmail.com>
2024-06-24 04:03:46 +0000

    Initialize Spring Boot app

---
+++ sig-vhe-backend/src/main/java/sn/esp/dgi/sig_vhe_backend/SigVheBackendApplication.java
@@ -0,0 +1,13 @@
+package sn.esp.dgi.sig_vhe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigVheBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigVheBackendApplication.class, args);
+	}
+
+}
---
+++ sig-vhe-backend/src/test/java/sn/esp/dgi/sig_vhe_backend/SigVheBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.esp.dgi.sig_vhe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigVheBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
