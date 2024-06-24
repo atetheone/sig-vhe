e6c1814731f3ce2e90a51fcde740ee26eec61f0e
atetheone <atevirran@gmail.com>
2024-06-24 21:36:46 +0000

    Refactor component selectors to not be standalone

--- sig-client/src/app/components/navbar/navbar.component.ts
+++ sig-client/src/app/components/navbar/navbar.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
--- sig-client/src/app/components/sidebar/sidebar.component.ts
+++ sig-client/src/app/components/sidebar/sidebar.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
