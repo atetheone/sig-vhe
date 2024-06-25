6c4307fb93b4be67974832a151615cc3af45d0d9
atetheone <atevirran@gmail.com>
2024-06-25 23:18:01 +0000

    chore: Update reusable components and styles for Angular Material theming

--- sig-client/src/app/components/footer/footer.component.spec.ts
+++ sig-client/src/app/components/footer/footer.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
 
@@ -6,12 +6,14 @@ describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FooterComponent]
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FooterComponent ]
     })
     .compileComponents();
-    
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- sig-client/src/app/components/footer/footer.component.ts
+++ sig-client/src/app/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  standalone: false,
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  test : Date = new Date();
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
