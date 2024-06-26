d4d9c7fa6d9e0e64e212e9f2647da1818a51e275
atetheone <atevirran@gmail.com>
2024-06-26 03:17:14 +0000

    Create HepatitisCase class

---
+++ sig-vhe-backend/src/main/java/sn/esp/dgi/sig_vhe_backend/service/PatientService.java
@@ -0,0 +1,27 @@
+package sn.esp.dgi.sig_vhe_backend.service;
+
+import sn.esp.dgi.sig_vhe_backend.domain.Patient;
+import sn.esp.dgi.sig_vhe_backend.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+  @Autowired
+  private PatientRepository repository;
+
+  public List<Patient> findAll() {
+    return repository.findAll();
+  }
+
+  public Patient save(Patient patient) {
+    return repository.save(patient);
+  }
+
+  public void deleteById(Long id) {
+    repository.deleteById(id);
+  }
+}
