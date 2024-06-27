167db6cfe78cbc3ceac559fd73ce0db95896a860
atetheone <atevirran@gmail.com>
2024-06-27 14:01:53 +0000

    Define PatientController

--- sig-vhe-backend/src/main/java/sn/esp/dgi/sig_vhe_backend/controller/PatientController.java
+++ sig-vhe-backend/src/main/java/sn/esp/dgi/sig_vhe_backend/controller/PatientController.java
@@ -1,4 +1,37 @@
 package sn.esp.dgi.sig_vhe_backend.controller;
 
+
+import sn.esp.dgi.sig_vhe_backend.domain.Patient;
+import sn.esp.dgi.sig_vhe_backend.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
 public class PatientController {
+
+  @Autowired
+  private PatientService service;
+
+  @GetMapping
+  public List<Patient> getAllPatients() {
+    return service.findAll();
+  }
+
+  @PostMapping
+  public Patient createPatient(@RequestBody Patient patient) {
+    return service.save(patient);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deletePatient(@PathVariable Long id) {
+    service.deleteById(id);
+  }
+
+  @GetMapping("/{id}")
+  public Patient getPatientById(@PathVariable Long id) {
+    return service.findById(id);
+  }
 }
