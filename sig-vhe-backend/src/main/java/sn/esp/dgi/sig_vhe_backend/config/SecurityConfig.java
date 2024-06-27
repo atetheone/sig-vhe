1b8e024b5711498b5ef3277a12cf822adb1d80cd
atetheone <atevirran@gmail.com>
2024-06-27 14:05:37 +0000

    Define security config classes

---
+++ sig-vhe-backend/src/main/java/sn/esp/dgi/sig_vhe_backend/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package sn.esp.dgi.sig_vhe_backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import sn.esp.dgi.sig_vhe_backend.jwt.JwtAuthenticationEntryPoint;
+import sn.esp.dgi.sig_vhe_backend.jwt.JwtAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+  @Autowired
+  private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+  @Autowired
+  private JwtAuthenticationFilter jwtAuthenticationFilter;
+  @Autowired
+  private UserDetailsService userDetailService;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //configuration
+    http.csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.disable())
+            .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.
+                    requestMatchers("/api/patients/**").authenticated().
+                    requestMatchers("/api/admin/**").hasRole("ADMIN").
+                    requestMatchers("/api/admin/all-users").permitAll().
+                    requestMatchers("/api/register").permitAll().
+                    anyRequest().authenticated())
+            .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(jwtAuthenticationEntryPoint)) // if any exception came
+            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // nothing to save on server
+    http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
+  @Bean
+  public DaoAuthenticationProvider doDaoAuthenticationProvider() {
+    DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+    daoAuthenticationProvider.setUserDetailsService(userDetailService);
+    daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+    return daoAuthenticationProvider;
+  }
+
+}
