108dc347ebef3a3d11a55e72e80fb8d2f7f44619
atetheone <atevirran@gmail.com>
2024-06-28 23:30:02 +0000

    Create HepatitisCaseRepository interface

---
+++ sig-vhe-backend/src/main/java/sn/esp/dgi/sig_vhe_backend/repository/HepatitsCaseRepository.java
@@ -0,0 +1,9 @@
+
+package sn.esp.dgi.sig_vhe_backend.repository;
+
+
+import sn.esp.dgi.sig_vhe_backend.domain.HepatitisCase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HepatitsCaseRepository extends JpaRepository<HepatitisCase, Long> {
+}
